bcb2c1242a082308f6e30aa1295a8b20c76de8dd
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2022-06-07 18:56:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {
   doc,
   arrayUnion,
   updateDoc,
-  onSnapshot
+  onSnapshot,
 } from "firebase/firestore";
 import { Button } from "@nextui-org/react";
 import confetti from "canvas-confetti";
@@ -19,13 +19,18 @@ import confetti from "canvas-confetti";
 function App() {
   const [msg, setMsg] = useState({});
   const [viewMsg, setViewMsg] = useState([]);
+  const [lastMsg, setLastMsg] = useState();
 
   const msgStore = {};
 
   const sendMsg = async () => {
-    await updateDoc(doc(db, "msgStore","messages","sandesh","suchana"), {
-      msgArray:arrayUnion(msg)
-    },{merge:true})
+    await updateDoc(
+      doc(db, "msgStore", "messages", "sandesh", "suchana"),
+      {
+        msgArray: arrayUnion(msg),
+      },
+      { merge: true }
+    )
       .then(console.log("Send Successfully"))
       .catch((error) => {
         alert(error.message);
@@ -33,35 +38,49 @@ function App() {
   };
 
   const fetch = async () => {
-
- onSnapshot(doc(db, "msgStore","messages","sandesh","suchana"),(parameter)=>(setViewMsg(parameter.data())));
-console.log(viewMsg);  
+    onSnapshot(
+      doc(db, "msgStore", "messages", "sandesh", "suchana"),
+      (parameter) => setViewMsg(parameter.data())
+    );
+    console.log(viewMsg);
     // console.log(data.docs);
     // setViewMsg(data.docs.map((elem) => ({ ...elem.data(), id: elem.id })));
   };
 
+  const handleChange = (e) => {
+    setMsg({ ...msg, message: e.target.value });
+    sendMsg();
 
-  const handleChange=(e)=>{
-    setMsg({...msg, message:e.target.value})
-    sendMsg()
-  }
+    setLastMsg(viewMsg?.msgArray.length);
+  };
 
+  console.log(viewMsg?.msgArray[lastMsg].message);
+
+  
   useEffect(() => {
     fetch();
   }, []);
   return (
-    <div className="h-screen w-full flex items-center justify-center ">
+    <div className="h-screen w-full flex flex-col items-center justify-center ">
       <div className="h-[500px] w-[500px] border-2 relative  ">
         <div className="p-10 w-full h-full gap-3 overflow-scroll">
-          {
-              viewMsg?.msgArray?.map((elem)=>
-            (  <span >
+          {/* {
+              viewMsg?.msgArray?.map((elem,ind)=>
+            (  <p >
               {elem.message}
-              </span>)
+              </p>)
             )
-          }
-         
+
+           
+          } */}
+
+          {/* <p>
+             {
+            viewMsg?.msgArray[lastMsg].message
+            }</p> */}
         </div>
+
+
 
         <div className="border-2  absolute bottom-0 w-full flex">
           {" "}
